fb2bc6981367235d19d1b8af789926ec2ff3e2c3
Nisa Üstündağ <nisaustundag070@gmail.com>
2024-12-15 13:42:13 +0300

    ilk commit

---
+++ sale.art.app/lib/models/Product.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+const String dummyText = "Bu ürün için açıklama burada yer alacak.";
+
+class Product {
+  final String? image,
+      title,
+      description; // 'descripton' yerine 'description' olmalı
+  final int? price, size, id;
+  final Color? color;
+
+  Product({
+    this.id,
+    this.image,
+    this.title,
+    this.price,
+    this.description, // Bu alan 'description' ile eşleşiyor
+    this.size,
+    this.color,
+  });
+}
+
+List<Product> products = [
+  Product(
+      id: 1,
+      title: "Comlek",
+      size: 12,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/coomlek.jpg",
+      color: Color(0xFF3D82AE)),
+  Product(
+      id: 2,
+      title: "Gözlükler",
+      size: 8,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/gözlük.jpg",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 3,
+      title: "Gramafon",
+      size: 10,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/gramafon.jpg",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 3,
+      title: "Halılar",
+      size: 11,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/halıjpg.jpg",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 4,
+      title: "Tabaklar",
+      size: 12,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/tabak.jpg",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 5,
+      title: "Telefonlar",
+      size: 12,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/telefon.jpg",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 6,
+      title: "Vazolar",
+      size: 11,
+      price: 234,
+      description: dummyText,
+      image: "sale.art.app/assets/images/vazo.jpg",
+      color: Color(0xFFD3A984)),
+];
